6a983832171135cebd964deac599f2af24b267d4
Bruno Caballero <bruno.caballero@gmail.com>
2024-12-02 20:39:50 +0100

    Day 2.

---
+++ 02/p22.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+
+"Solution for exercise 1.1"
+
+import sys;
+
+count = 0
+
+def check_direction(levels):
+    if len(levels) < 2:
+        return False
+    if (levels[1] > levels[0]):
+        ascending = True
+    elif (levels[1] < levels[0]):
+        ascending = False
+    else:
+        return False    
+    for i in range(1,len(levels)):
+        if ascending and levels[i] <= levels[i-1]:
+            return False
+        elif not(ascending) and levels[i] >= levels[i-1]:
+            return False
+    return True
+
+
+def check_increase(levels):  
+    for i in range(1,len(levels)):
+        diff = abs(levels[i]-levels[i-1])
+        if diff < 1 or diff > 3:
+            return False
+    return True
+
+with open('input2.txt', 'r') as file:
+    for line in file:
+        levels = []
+        groups = line.split()
+        for i in range(0,len(groups)):
+            levels.append(int(groups[i]))
+        
+        for i in range(0, len(groups)):
+            levels2 = levels.copy()
+            levels2.pop(i)
+            if check_direction(levels2) and check_increase(levels2):
+                count = count +1
+                break
+
+print(count)
+        
+                
